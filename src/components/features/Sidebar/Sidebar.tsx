231cb8ad5948265e9d28e1dfcc412e6bab74aaf0
nanahiryu <ryu2751x7@gmail.com>
2023-04-16 13:14:50 +0900

    sidebar設置(一時的コミット)

---
+++ src/components/features/Sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const HamburgerContainer = styled.div`
+  padding: 20px 20px 0 20px;
+`;
+
+export const Sidebar = () => {
+  return (
+    <>
+      <HamburgerContainer></HamburgerContainer>
+    </>
+  );
+};
