ba27a00c4059b1810473fa4f0778e4f88db81f82
nanahiryu <ryu2751x7@gmail.com>
2023-04-16 13:14:50 +0900

    sidebar設置(一時的コミット)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/pages/Home";
+import { Sidebar } from "./components/features/Sidebar/Sidebar";
+
+const SidebarContainer = styled.div`
+  width: 80px;
+  height: 100%;
+  background-color: #878787;
+  position: fixed;
+`;
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <SidebarContainer>
+        <Sidebar />
+      </SidebarContainer>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
