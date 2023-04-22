596e03565ba09d71f3787e64349b8e5dc6258260
nanahiryu <ryu2751x7@gmail.com>
2023-04-22 14:45:27 +0900

    不要なimportの削除

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, Icon, Image } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { FaRegEdit } from "react-icons/fa";
 import { NormalModeField } from "../features/Home/NormalModeField";
